ac65657068c27914781324a239056ccc61b18eba
miurakazuma67 <miurakazuma67@gmail.com>
2021-02-18 00:02:25 +0900

    ファイルを更新(1回目)

--- TaskPart2/ViewController.swift
+++ TaskPart2/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var label: UILabel!
+    
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    
+    
+    @IBAction func culculate(_ sender: Any) {
+        let num1 = Double(textField1.text ?? "" ) ?? 0
+        let num2 = Double(textField2.text ?? "" ) ?? 0
+        var result: Double = 0
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            result = num1 + num2
+        case 1:
+            result = num1 - num2
+        case 2:
+            result = num1 * num2
+        case 3:
+            guard num2 != 0 else {
+                label.text = "割る数には0以外の数字を入力してください"
+                return
+            }
+            result = num1 / num2
+            
+        default:
+            return
+        }
+        label.text = String(result)
     }
-
-
 }
 
